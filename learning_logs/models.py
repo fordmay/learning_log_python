b444ddd3f9eceda4cef25127e98e8715aaa804d1
Dmytro Zheliabin <d.u.zheliabin@gmail.com>
2020-12-27 23:23:25 +0200

    Defining the Entry Model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,21 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        entry = self.text[:50]
+        if len(entry) < 50:
+            return f"{entry}"
+        else:
+            return f"{entry}..."
